30963908030448d5e4697d23e60ba024fe2cb815
Justin <jspivack2198@gmail.com>
2023-08-30 13:42:17 -0400

    new push

---
+++ app.js
@@ -0,0 +1,23 @@
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+var e = 5
+var f = 6
+var g = 7
+var h = 8
+var i = 9
+var j = 10
+
+console.log(d + e);
+console.log(a + b);
+console.log(j - i);
+console.log(j - e);
+console.log(a * d);
+console.log(a * h);
+console.log(j / b);
+console.log(h / d);
+console.log(a += 1);
+console.log(j += 2);
+console.log(j -= 5);
+console.log(e -= 3)
